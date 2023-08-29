cd94ffc53f41d72d69731dcbda8390e74ced75d4
Blake <blake.anneberg@gmail.com>
2023-08-28 20:33:15 -0600

    got confused, need to read instructions more

---
+++ combat_advantage.py
@@ -0,0 +1,22 @@
+player_power = 101
+enemy_defense = 100
+advantage, disadvantage, evenly_matched = False, False, False
+
+# Do not touch above this line, this is where I got confused... should have read the instructions
+
+if player_power > enemy_defense:
+    advantage = True
+    
+elif enemy_defense < player_power:
+    disadvantage = True
+else: 
+    evenly_matched = True
+
+# Do not touch below this line
+
+if advantage:
+    print("You have the advantage!")
+elif disadvantage:
+    print("You have the disadvantage!")
+else:
+    print("You are evenly matched!")

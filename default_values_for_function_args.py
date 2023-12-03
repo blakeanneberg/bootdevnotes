80bc5fb921d16da4576a8a495d59caa754c21a95
root <blake.anneberg@gmail.com>
2023-12-03 14:01:52 -0700

    more notes

---
+++ default_values_for_function_args.py
@@ -0,0 +1,36 @@
+def get_punched(health, armor=0):
+    # ?
+    damage = 50 - armor
+    health = health - damage
+    return health
+
+
+def get_slashed(health, armor=0):
+    # ?
+    damage = 100 - armor
+    health = health - damage
+    return health
+
+
+# Don't touch below this line
+
+
+def test(health, armor):
+    print(f"Health: {health}, Armor: {armor}")
+    print(f"Health after punch: {get_punched(health, armor)}")
+    print("=====================================")
+    print(f"Health: {health}, Armor: {armor}")
+    print(f"Health after slash: {get_slashed(health, armor)}\n")
+    print("=====================================")
+    print(f"Health: {health}, Armor: no armor!")
+    print(f"Health after slash: {get_slashed(health)}\n")
+    print("=====================================")
+    print(f"Health: {health}, Armor: no armor!")
+    print(f"Health after punch: {get_punched(health)}")
+    print("=====================================")
+
+
+test(400, 5)
+test(300, 3)
+test(200, 1)
+

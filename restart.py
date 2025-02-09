5bd1e8a3ee603d384a832d9d143fa1da8b7c78f2
root <blake.anneberg@gmail.com>
2025-02-09 15:13:45 -0700

    relearning

---
+++ restart.py
@@ -0,0 +1,8 @@
+my_name = "Will Shakespear"
+
+print("Our doubts are traitors, and make us lose the good we oft might win, by fearing to attempt.")
+
+print(my_name) 
+
+
+Stopped CH2 Variables L3 storing results

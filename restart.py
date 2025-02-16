008ca5257a87bff48cc252939643f771e74775d7
root <blake.anneberg@gmail.com>
2025-02-16 14:08:32 -0700

    time for swim

--- restart.py
+++ restart.py
@@ -1,10 +1,9 @@
-my_name = "Will Shakespear"
-
-print("Our doubts are traitors, and make us lose the good we oft might win, by fearing to attempt.")
-
-print(my_name) 
-
-## Stopped CH2 Variables L3 storing results
+def main():
+    my_name = "Will Shakespear"
+    f_string = f"Our doubts are traitors, and make us lose the good we oft might win, by fearing to attempt. by {my_name} "
+    print(f_string)
+    print("I want to document all that I learn here") 
+main()
 
 
 

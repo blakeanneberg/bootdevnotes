6a8b4fa73eda073e14489d2caef4b244c4ed164f
Blake <blake.anneberg@gmail.com>
2026-02-08 13:44:32 -0700

    adding more files

---
+++ workspace/BlakeAnneberg/BookBot/README.md
@@ -0,0 +1,3 @@
+# bookbot
+
+BookBot is my first [Boot.dev](https://www.boot.dev) project!
---
+++ workspace/BlakeAnneberg/BookBot/main.py
@@ -0,0 +1,11 @@
+def main():
+    filepath = "books/frankenstein.txt"
+    book_text = get_book_text(filepath)
+    print (book_text)
+def get_book_text(filepath):
+    with open(filepath) as f:
+        text = f.read() 
+    return text
+main()
+
+

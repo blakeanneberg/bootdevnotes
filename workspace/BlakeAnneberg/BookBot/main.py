9d8126b694de027e03505d53fb092959a309c46d
root <blake.anneberg@gmail.com>
2026-02-08 14:39:58 -0700

    more notes

--- workspace/BlakeAnneberg/BookBot/main.py
+++ workspace/BlakeAnneberg/BookBot/main.py
@@ -1,11 +1,13 @@
 def main():
     filepath = "books/frankenstein.txt"
     book_text = get_book_text(filepath)
-    print (book_text)
+    print(book_text)
+
+
 def get_book_text(filepath):
     with open(filepath) as f:
-        text = f.read() 
+        text = f.read()
     return text
+
+
 main()
-
-
